456b62e9b1667c576973c1d23de5a09fa4c20700
Erik Williams <erikmartinez.009@gmail.com>
2022-03-15 21:05:21 -0700

    Hello

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  fight();
